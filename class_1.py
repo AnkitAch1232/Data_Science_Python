6faf61b6ef60aa43d15c0c5c6725cd84da9f6131
Ankit Acharya <ankitacharya32123@gmail.com>
2025-06-13 08:13:40 +0545

    creating data structure

---
+++ class_1.py
@@ -0,0 +1,23 @@
+#data structures
+#List
+my_list1 = [1,2,3,4]
+my_list2 = ["ankit","janak","sunil"]
+#Tuple
+my_tuple = (1,2,3,4)
+my_tuple2 = ("ankit","janak","sunil")
+#Set
+my_set = {1,2,3,4}
+my_set2 = {"ankit","janak","sunil"}
+#Dictinory
+my_dictinory1 = {"a":22,"b":23,"c":24}
+my_dictinory2 = {"ankit":1, "janak":2,"sunil":3}
+
+#Print
+print(my_list1)
+print(my_list2)
+print(my_tuple)
+print(my_tuple2)
+print(my_set)
+print(my_set2)
+print(my_dictinory1)
+print(my_dictinory2)
